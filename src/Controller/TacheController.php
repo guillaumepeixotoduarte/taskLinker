dd43ea991fecc054660e08155b007be8d0b531c3
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-03-05 11:54:45 +0100

    Ajout partie tache

---
+++ src/Controller/TacheController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Projet;
+use App\Entity\Tache;
+use App\Form\TacheType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TacheController extends AbstractController
+{
+    #[Route('/tache/new/{projetId}', name: 'app_tache_add')]
+    public function add(int $projetId, Request $request, EntityManagerInterface $em): Response
+    {
+        $projet = $em->getRepository(Projet::class)->find($projetId);
+        $tache = new Tache();
+
+        $form = $this->createForm(TacheType::class, $tache);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tache->setProjet($projet); // Associez la tâche au projet
+            $em->persist($tache);
+            $em->flush();
+            $this->addFlash('success', 'La tâche a bien été créée');
+            return $this->redirectToRoute('app_projet_details', [
+                'id' => $projetId,
+            ]);
+        }
+
+        return $this->render('tache/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/tache/edit/{id}', name: 'app_tache_edit', methods: ['GET', 'POST'])]
+    public function edit(Tache $tache, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TacheType::class, $tache);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($tache);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Tâche mise à jour !');
+
+            return $this->redirectToRoute('app_projet_details', ['id' => $tache->getProjet()->getId()]);
+        }
+
+        return $this->render('tache/add.html.twig', [
+            'tache' => $tache,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/tache/delete/{id}', name: 'app_tache_delete', methods: ['POST'])]
+    public function delete(Request $request, Tache $tache, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $tache->getId(), $request->request->get('_token'))) {
+            $em->remove($tache);
+            $em->flush();
+            $this->addFlash('success', 'La tâche a bien été supprimée');
+        }
+
+        return $this->redirectToRoute('app_projet_details', [
+            'id' => $tache->getProjet()->getId(),
+        ]);
+    }
+}
---
+++ src/Form/TacheType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employe;
+use App\Entity\Tache;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TacheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'label' => 'Titre de la tâche',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+            ])
+            ->add('date', DateType::class, [
+                'label' => 'Date',
+            ])
+            ->add('statut', ChoiceType::class, [
+                'label' => 'Statut',
+                'choices' => [
+                    'To Do' => 'A_FAIRE',
+                    'Doing' => 'EN_COURS',
+                    'Done' => 'TERMINE',
+                ],
+            ])
+            ->add('employe', EntityType::class, [
+                'class' => Employe::class,
+                'choice_label' => function (Employe $employe) {
+                    return $employe->getPrenom() . ' ' . $employe->getNom();
+                },
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tache::class,
+        ]);
+    }
+}
