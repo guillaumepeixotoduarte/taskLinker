fc7423fe798b17aed1e833ac4e607d54f2479ea8
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-03-24 19:34:36 +0100

    Ajout des formulaires Inscription/Connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,39 +4,63 @@ namespace App\Controller;
 
 use App\Entity\Employe;
 use App\Form\RegistrationFormType;
+use App\Security\AppCustomAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    // #[Route('/register', name: 'app_register')] --- IGNORE ---
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        UserAuthenticatorInterface $userAuthenticator,
+        AppCustomAuthenticator $authenticator,
+        EntityManagerInterface $entityManager
+    ): Response {
         $user = new Employe();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
+            // 1. On récupère les données du champ non-mappé (le rôle)
+            $isChef = $form->get('isChefProjet')->getData();
+            if ($isChef === 'CHEF') {
+                $user->setRoles(['ROLE_CHEF_PROJET']);
+            } else {
+                $user->setRoles(['ROLE_USER']);
+            }
 
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            // 2. On hache le mot de passe (champ non-mappé plainPassword)
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // Note : Les champs 'nom', 'prenom', 'email', 'date_entree' et 'statut'
+            // sont automatiquement remplis dans $user par le $form->handleRequest($request)
+            // car ils sont "mappés" par défaut dans le RegistrationFormType.
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
-
-            return $this->redirectToRoute('list_employe');
+            // 3. Authentification automatique après l'inscription
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
         }
 
         return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,9 @@ use App\Entity\Employe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,30 +20,41 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('nom', TextType::class, ['label' => 'Nom'])
+            ->add('prenom', TextType::class, ['label' => 'Prénom'])
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
+            ->add('date_entree', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date d\'entrée',
+                'data' => new \DateTime(), // Par défaut aujourd'hui
+            ])
+            ->add('statut', ChoiceType::class, [
+                'choices'  => [
+                    'Actif' => 'ACTIF',
+                    'En congé' => 'CONGE',
+                    'Sortie' => 'SORTIE',
                 ],
             ])
+            // On ajoute le choix du rôle (en mode "simple" pour le formulaire)
+            ->add('isChefProjet', ChoiceType::class, [
+                'mapped' => false, // Ce champ n'existe pas dans l'entité, on le gèrera dans le contrôleur
+                'label' => 'Type de compte',
+                'choices'  => [
+                    'Employé classique' => 'USER',
+                    'Chef de Projet' => 'CHEF',
+                ],
+                'expanded' => true, // Boutons radio au lieu de liste déroulante
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [new IsTrue(['message' => 'Vous devez accepter les conditions.'])],
+            ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank(
-                        message: 'Please enter a password',
-                    ),
-                    new Length(
-                        min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        max: 4096,
-                    ),
+                    new NotBlank(['message' => 'Entrez un mot de passe']),
+                    new Length(['min' => 6, 'max' => 4096]),
                 ],
             ])
         ;
