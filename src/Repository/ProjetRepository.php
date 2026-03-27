a773dc9692cdb617939438c32699e22f66f7657d
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-03-27 12:34:55 +0100

    Requete projet selon user type

--- src/Repository/ProjetRepository.php
+++ src/Repository/ProjetRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Employe;
 use App\Entity\Projet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,6 +15,40 @@ class ProjetRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Projet::class);
+    }
+
+    public function findAllActiveForUser(Employe $user): array
+    {
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.archive = :archived')
+            ->setParameter('archived', false);
+
+        // Si l'utilisateur n'est PAS un chef de projet, on restreint aux projets où il est inscrit
+        if (!in_array('ROLE_CHEF_PROJET', $user->getRoles())) {
+            $qb->innerJoin('p.employes', 'e')
+            ->andWhere('e.id = :userId')
+            ->setParameter('userId', $user->getId());
+        }
+
+        // On peut ajouter un tri par date de création par exemple
+        $qb->orderBy('p.id', 'DESC');
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function isUserLinkedToProjet(int $projetId, int $userId): bool
+    {
+        $result = $this->createQueryBuilder('p')
+            ->select('COUNT(p.id)')
+            ->innerJoin('p.employes', 'e')
+            ->where('p.id = :projetId')
+            ->andWhere('e.id = :userId')
+            ->setParameter('projetId', $projetId)
+            ->setParameter('userId', $userId)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        return $result > 0;
     }
 
     //    /**
