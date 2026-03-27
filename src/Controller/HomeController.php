ea0e009c8855b73032676a45ad30f8c535a5ec18
guillaumepeixotoduarte <guillaume.peixotoduarte@gmail.com>
2026-03-27 12:34:28 +0100

    Redirection si utilisateur déja connecté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,10 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_projet');
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
